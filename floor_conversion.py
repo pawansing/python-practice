93b3cc4c5a7ec3e33d254579eb865ff100e0e746
pawan <ujjain.pawansingh@gmail.com>
2019-04-01 03:45:26 +0200

    1st commit

---
+++ floor_conversion.py
@@ -0,0 +1,8 @@
+# Enter floor level in europe
+floor_in_eu = input('Floor in EU')
+
+# Convert floor level as per USA conventions
+floor_in_usa = int(floor_in_eu)+1
+
+# Get floor level as USA
+print('US floor', floor_in_usa)
